31be5b4bb0e49128fc229769d6635eb32f8712e6
fallingify <gnswldl000@gmail.com>
2025-05-30 15:11:56 +0900

    feat: health check

---
+++ src/main/java/flgfy/qrlogi/health/HealthCheckController.java
@@ -0,0 +1,13 @@
+package flgfy.qrlogi.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/api/health")
+    public String healthCheck() {
+        return "정상 동작합니다.";
+    }
+}
