cb5d6ca10f8828b4d3f120f352f6a61e657e2817
fallingify <gnswldl000@gmail.com>
2025-07-03 16:42:45 +0900

    구글시트 연동 구현 진행: GoogleSheetService setting

--- api/src/main/java/com/qrlogi/api/QrLogiApplication.java
+++ api/src/main/java/com/qrlogi/api/QrLogiApplication.java
@@ -3,14 +3,17 @@ package com.qrlogi.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.qrlogi")
 @EntityScan("com.qrlogi.domain")
+@EnableFeignClients("com.qrlogi.domain")
 @EnableJpaRepositories("com.qrlogi.domain")
 @EnableJpaAuditing
 public class QrLogiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(QrLogiApplication.class, args);
     }
